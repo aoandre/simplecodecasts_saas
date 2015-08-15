a371d0ec2eae87ec1ad5e58600578aeae2ab11db
Alfonso Andre <aoandre8@hotmail.com>
2015-08-15 22:48:20 +0000

    Corrected contact_email.rb

--- simplecodecasts_saas/app/mailers/contact_mailer.rb
+++ simplecodecasts_saas/app/mailers/contact_mailer.rb
@@ -1,5 +1,5 @@
 class ContactMailer < ActionMailer::Base
-    default to: 'aoandre8@hotmail.com', 'pchardwaremasters@gmail.com'
+    default to: 'aoandre8@hotmail.com'
     
     def contact_email(name, email, body)
         @name = name
